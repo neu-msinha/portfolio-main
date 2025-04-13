a0fc69c40b601a6fd78cb68cefa4c33c56e2ad77
Mayukh Sinha <sinha.may@northeastern.edu>
2025-04-12 23:53:17 -0400

    Initial Project

---
+++ src/containers/skillProgress/skillProgress.js
@@ -0,0 +1,49 @@
+import React, {useContext} from "react";
+import "./Progress.scss";
+import {illustration, techStack} from "../../portfolio";
+import {Fade} from "react-reveal";
+import Build from "../../assets/lottie/build";
+import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function StackProgress() {
+  const {isDark} = useContext(StyleContext);
+  if (techStack.viewSkillBars) {
+    return (
+      <Fade bottom duration={1000} distance="20px">
+        <div
+          className={isDark ? "dark-mode skills-container" : "skills-container"}
+        >
+          <div className="skills-bar">
+            <h1 className="skills-heading">Proficiency</h1>
+            {techStack.experience.map((exp, i) => {
+              const progressStyle = {
+                width: exp.progressPercentage
+              };
+              return (
+                <div key={i} className="skill">
+                  <p>{exp.Stack}</p>
+                  <div className="meter">
+                    <span style={progressStyle}></span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="skills-image">
+            {illustration.animated ? (
+              <DisplayLottie animationData={Build} />
+            ) : (
+              <img
+                alt="Skills"
+                src={require("../../assets/images/skill.svg")}
+              />
+            )}
+          </div>
+        </div>
+      </Fade>
+    );
+  }
+  return null;
+}
